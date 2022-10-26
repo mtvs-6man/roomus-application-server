afca8e00d3ede47cfc8f2903c1e4b52ff2651a92
5onchangwoo <scw9410@gmail.com>
2022-10-26 20:19:11 +0900

    fist commit

---
+++ src/test/java/com/sixman/roomus/RoomusApplicaitonSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixman.roomus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomusApplicaitonSeverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
