a602611b475bac7761def37a5476f63ff8e7dd59
5onchangwoo <scw9410@gmail.com>
2022-11-04 09:23:10 +0900

    Feat: "ResponseDTO" 추가
    API 응답을 위한 ResponseDTO 기능 추가

---
+++ src/main/java/com/sixman/roomus/commons/dto/ResponseDTO.java
@@ -0,0 +1,15 @@
+package com.sixman.roomus.commons.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ResponseDTO {
+    private HttpStatus statusCode;
+    private String message;
+    private Object date;
+}
