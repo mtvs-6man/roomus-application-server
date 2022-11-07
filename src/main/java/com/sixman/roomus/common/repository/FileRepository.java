c7ee503d9a913748f97531d93a6a13dd039725e9
5onchangwoo <scw9410@gmail.com>
2022-11-07 23:38:23 +0900

    [#13] Remove: 바뀌기전 s3 관련 코드 삭제

--- src/main/java/com/sixman/roomus/common/repository/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sixman.roomus.common.repository;
-
-import com.sixman.roomus.common.model.File;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<File, Integer> {
-}
