bb2042d099788fe3a0dcf2c08943b78054b6fbeb
lacred <lacred@isang-us-MacBook-Pro.local>
2022-11-12 22:46:24 +0900

    <fix> : jenkins sh file modify

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,14 @@
 # 가동중인 awsstudy 도커 중단 및 삭제
-sudo docker ps -a -q --filter "name=mungjin4966" | grep -q . && docker stop awsstudy && docker rm awsstudy | true
+sudo docker ps -a -q --filter "name=roomusApplication" | grep -q . && docker stop roomusApplication && docker rm roomusApplication | true
 
 # 기존 이미지 삭제
-sudo docker rmi backtony/awsstudy:1.0
+sudo docker rmi mungjin4966/roomus_app:1.0
 
 # 도커허브 이미지 pull
-sudo docker pull backtony/awsstudy:1.0
+sudo docker pull mungjin4966/roomus_app:1.0
 
 # 도커 run
-docker run -d -p 8000:8000 --name awsstudy backtony/awsstudy:1.0
+docker run -d -p 80:8000 --name roomusApplication mungjin4966/roomus_app:1.0
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
 docker rmi -f $(docker images -f "dangling=true" -q) || true
