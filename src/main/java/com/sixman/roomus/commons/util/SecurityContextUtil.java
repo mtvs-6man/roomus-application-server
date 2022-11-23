766a5e013221469bb55385b602cdeb3d4b08fd48
5onchangwoo <scw9410@gmail.com>
2022-11-23 16:12:53 +0900

    [#64] Feat: "SecurityContextUtil" 추가
    SecurityContextHolder 안에 있는 정보를 꺼내오기 위한 util 객체 추가

--- src/main/java/com/sixman/roomus/commons/util/SecurityContextUtil.java
+++ src/main/java/com/sixman/roomus/commons/util/SecurityContextUtil.java
@@ -1,13 +1,20 @@
+
 package com.sixman.roomus.commons.util;
 
-import com.sixman.roomus.member.command.domain.model.Member;
+import com.sixman.roomus.config.auth.PrincipalDetails;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class SecurityContextUtil {
+
     public Integer getMemberNo() {
-        Member loggedMember = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return loggedMember.getMemberNo();
+        PrincipalDetails principal = (PrincipalDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        System.out.println("현재 로그인한 유저 : " + principal.getMember().getMemberNo());
+
+        return principal.getMember().getMemberNo();
     }
 }
+
